9c5142eae163e73baabfa5f9ee9226e89ad2ef6f
cherryTec <18911422592@189.cn>
2021-12-17 14:06:03 +0000

    Add GET and POST endpoint

--- src/lib/todo-item.svelte
+++ src/lib/todo-item.svelte
@@ -1,4 +1,8 @@
-<style>
+<script lang="ts">
+    export let todo: Todo;
+  </script>
+
+<style>    
     .todo{
        display: grid;
        grid-template-columns: 2rem 1fr 2rem;
@@ -77,7 +81,7 @@
     </form>
 
     <form action="" method=""  class="text">
-        <input type="text">
+        <input type="text" value="{todo.text}">
         <button aria-label="Save todo" class="save"></button>
     </form>
 
