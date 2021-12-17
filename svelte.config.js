a89ea5362a5acc207b43a01095c6df737f4636d7
cherryTec <18911422592@189.cn>
2021-12-17 06:38:43 +0000

    fix sever bug 2

--- svelte.config.js
+++ svelte.config.js
@@ -14,13 +14,13 @@ const config = {
 		target: '#svelte',
 		vite: {
 			server: {
-				
 				hmr: {
 					clientPort: process.env.HMR_HOST ? 443: 3000,
-					host: process.env.HMR_HOST ? process.env.HMR_HOST.substring('https://'.length) : "localhost"
+					host: process.env.HMR_HOST ? process.env.HMR_HOST.substring("https://".length) : "localhost"
 				}
 			}
 		}
+	
 	}
 };
 
