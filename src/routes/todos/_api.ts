7d2ff213bbb291577d80a02df3870cdf613df2b7
cherryTec <18911422592@189.cn>
2021-12-18 12:11:40 +0000

    Ehance HTML forms.

--- src/routes/todos/_api.ts
+++ src/routes/todos/_api.ts
@@ -25,6 +25,7 @@ export const api = (request: RequestHandler, data?: Record<string, unknown>) => 
         case  "DELETE":
           todos = todos.filter(todo => todo.uid !== request.params.uid)
           status: 200;
+          
           break;
       
         case "PATCH":
@@ -36,6 +37,7 @@ export const api = (request: RequestHandler, data?: Record<string, unknown>) => 
               return todo;
             });
           status = 200;
+          body = todos.find(todo => todo.uid === request.params.uid);
           break;  
 
 
@@ -43,7 +45,8 @@ export const api = (request: RequestHandler, data?: Record<string, unknown>) => 
             break;
     }
 
-    if(request.method.toUpperCase() !== "GET") {
+    if(request.method.toUpperCase() !== "GET" && 
+        request.headers.accept !== "application/json") {
       return {
         status:303,
         headers: {
